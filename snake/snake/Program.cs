d03ce1245b66aea6ab66fe24199fd330472b2167
vadikat <vadim.dema@gmail.com>
2016-07-26 15:27:57 +0400

    создали вертикальную линию и рамку

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,15 +10,25 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
+            // Отрисовка точек
+                
+            Point p = new Point(4, 5, '*');
+            p.Draw();
 
-             Console.ReadLine();
-        }
+            Console.ReadLine();
+         }
 
     }
 }
--- snake/snake/VerticalLine.cs
+++ snake/snake/VerticalLine.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace snake
 {
-    class HorizontalLine : Figure
+    class VerticalLine : Figure
     {
-        
-
-        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+       
+        public VerticalLine(int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
-            for (int x = xLeft; x <= xRight; x++)
+            for (int y = yUp; y <= yDown; y++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
-            
+
         }
 
         
+
     }
 }
