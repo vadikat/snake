61b9fd51a6fb982965c78df83a9a3f0272095903
vadikat <vadim.dema@gmail.com>
2016-07-26 12:58:36 +0400

    Horizontal Line

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,6 +16,8 @@ namespace snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
 
              Console.ReadLine();
         }
