76bb8c6d8d2fd94975ddcacece5c7dd46ced3c44
vadikat <vadim.dema@gmail.com>
2023-03-16 20:24:35 +0200

    creat class

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,23 +10,22 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);
+            point p1 = new point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            point p2 = new point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
 
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y , char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+      
     }
 }
